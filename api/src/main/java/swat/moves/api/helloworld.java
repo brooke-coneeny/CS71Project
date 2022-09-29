3720829e7082af0e9893157f8ecdb1604eab37ed
Will Bernstein <willbernstein@dhcp-130-58-102-167.swarthmore.edu>
2022-09-29 10:07:16 -0400

    hello world controller

--- api/src/main/java/swat/moves/api/helloworld.java
+++ api/src/main/java/swat/moves/api/helloworld.java
@@ -1,19 +1,14 @@
 package swat.moves.api;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Controller
+@RestController
+@RequestMapping
 public class helloworld {
 
-  private static final String template = "Hello World";
     @GetMapping("/hello")
-    public String hello()   
-    {  
-    return "Hello Team 5";  
-    }  
+    public String hello(){
+      return "hello World";
+    }
   }
