176ff51b700b2529294ef99a8fa296336410294c
orhun-kolgeli <okolgel1@swarthmore.edu>
2022-10-07 15:36:40 -0400

    Retrieve user selections

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,28 @@
-var miles;
+$("#confirm").click(function() {
+    var miles = $('#miles').val();
+    var location = $('#dropdownMenuButton').text();
+    alert(
+        "Miles: " + String(miles) + "\n" +
+        "Location: " + location
+    );
+});
 
-$("#confirm-miles").click(function() {
-    miles = $('#input-miles').val();
-    alert(miles);
+$("#science-center").click(function() {
+    $('#dropdownMenuButton').text("Science Center");
+});
+
+$("#parrish").click(function() {
+    $('#dropdownMenuButton').text("Parrish");
+});
+
+$("#ml").click(function() {
+    $('#dropdownMenuButton').text("Mary Lyons");
+});
+
+$("#willets").click(function() {
+    $('#dropdownMenuButton').text("Willets");
+});
+
+$("#nppr").click(function() {
+    $('#dropdownMenuButton').text("PPR Apartments");
 });
