6c21cee3709a6ba345519e0bb43b0c3d4a0d3927
Alex Huynh <alexh3925@dhcp-130-58-98-236.swarthmore.edu>
2022-10-24 00:20:30 -0400

    js file for backend interaction

---
+++ scripts/request.js
@@ -0,0 +1,14 @@
+/*
+function sendRequest() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        // Do something here with this.responseText
+        console.log("Response Received: " + this.responseText);
+      }
+    };
+    xhttp.open("GET", "data/mydata.txt", true);
+    xhttp.send();
+    console.log("Request Sent");
+  }
+  */
